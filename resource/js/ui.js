c84818fe0d79e838d6ff342b38bc2f33c823aa5f
bsk824 <bsk824@gmail.com>
2019-07-24 16:51:03 +0900

    backup

--- resource/js/ui.js
+++ resource/js/ui.js
@@ -1,30 +1,33 @@
 var objDefault = {
-	'win' : $(window),
-	'doc' : $('html'),
-	'body' : $('body'),
-	'cont' : $('#container')
+	'win': $(window),
+	'doc': $('html'),
+	'body': $('body'),
+	'cont': $('#container')
 }
 function pageLoad(page) {
 	$.ajax({
-		url : page + '.html',
+		url: page + '.html',
 		dataType: 'html',
-		success : function(data){
+		error: function(r,s,e){
+			if(s == 'error') {
+				console.log(s);
+				return false;
+			}
+		},
+		success: function(data){
+
 			if(page.indexOf('brands/') >= 0) {
 				objDefault.body.addClass('brands');
+				var brandName = page.split('/')[1];
+				$('.brandMenu li').each(function(){
+					var $this = $(this);
+					if($this.data('brand') == brandName) $this.addClass('active').siblings().removeClass('active');
+				})
 			} else {
 				objDefault.body.removeClass('brands');
 			}
-			
-			objDefault.cont.html(data);
-			var imgLeng = 0;
-			var leng = objDefault.cont.find('img').length;
-			objDefault.cont.find('img').on('load', function(){
-				imgLeng++;
-				if(imgLeng == leng) {
-					objDefault.cont.removeClass().addClass(page);
-					objSet();
-				}
-			});
+
+			objDefault.cont.html(data).find('img').on('load', function(){ objSet(); });
 		}
 	});
 }
@@ -75,7 +78,9 @@ function roll(obj, dir) {
 			} else {
 				style[dir] = pos + 'px';
 			}
+
 			imgWrap.css(style);
+
 		},30);
 	});
 }
@@ -87,39 +92,44 @@ var endObjPos = {};
 function objSet() {
 	objInfoArry.forEach(function(obj){
 		var $this = $('.' + obj.name);
-		var start = $this.offset().top;
-		var end = start + $this.height();
-		movObj[obj.name] = $('.' + obj.name + ' .movObj');
-		startObjPos[obj.name] = start;
-		endObjPos[obj.name] = end; 
-		if(obj.startCls) {fixedWrap[obj.name] = $('.' + obj.name + ' .fixedWrap')}
+		if($this.length) {
+			var start = $this.offset().top;
+			var end = start + $this.height();
+			movObj[obj.name] = $('.' + obj.name + ' .movObj');
+			startObjPos[obj.name] = start;
+			endObjPos[obj.name] = end; 
+			if(obj.startCls) {fixedWrap[obj.name] = $('.' + obj.name + ' .fixedWrap')}
+		}
 	});
 }
 function scrollObj(obj, scrollTop, screenEnd) {
+	var $this = $('.' + obj.name);
 	var style = {};
-	if(obj.startCls) {
-		if(obj.endCls) {
-			(startObjPos[obj.name] < scrollTop && endObjPos[obj.name] > screenEnd) ? fixedWrap[obj.name].addClass(obj.startCls) : fixedWrap[obj.name].removeClass(obj.startCls);
-			(endObjPos[obj.name] <= screenEnd) ? fixedWrap[obj.name].addClass(obj.endCls) : fixedWrap[obj.name].removeClass(obj.endCls);
-		} else {
-			(startObjPos[obj.name] < scrollTop) ? fixedWrap[obj.name].addClass(obj.startCls) : fixedWrap[obj.name].removeClass(obj.startCls);
-		}
-	} else {
-		if(startObjPos[obj.name] < screenEnd && endObjPos[obj.name] > scrollTop) {
-			if(obj.topStart == true) {
-				var posY = ((scrollTop - startObjPos[obj.name]) / (endObjPos[obj.name] - startObjPos[obj.name]) * obj.y);
-				var posX = ((scrollTop - startObjPos[obj.name]) / (endObjPos[obj.name] - startObjPos[obj.name]) * obj.x);
+	if($this.length) {
+		if(obj.startCls) {
+			if(obj.endCls) {
+				(startObjPos[obj.name] < scrollTop && endObjPos[obj.name] > screenEnd) ? fixedWrap[obj.name].addClass(obj.startCls) : fixedWrap[obj.name].removeClass(obj.startCls);
+				(endObjPos[obj.name] <= screenEnd) ? fixedWrap[obj.name].addClass(obj.endCls) : fixedWrap[obj.name].removeClass(obj.endCls);
 			} else {
-				var posY = ((screenEnd - startObjPos[obj.name]) / (endObjPos[obj.name] - startObjPos[obj.name]) * obj.y) / 2;
-				var posX = ((screenEnd - startObjPos[obj.name]) / (endObjPos[obj.name] - startObjPos[obj.name]) * obj.x) / 2;
+				(startObjPos[obj.name] < scrollTop) ? fixedWrap[obj.name].addClass(obj.startCls) : fixedWrap[obj.name].removeClass(obj.startCls);
 			}
-			style['top'] = posY + 'px';
-			style['left'] = posX + 'px';
-		} else if(startObjPos[obj.name] >= screenEnd) {
-			style['top'] = 0;
-			style['left'] = 0;
+		} else {
+			if(startObjPos[obj.name] < screenEnd && endObjPos[obj.name] > scrollTop) {
+				if(obj.topStart == true) {
+					var posY = ((scrollTop - startObjPos[obj.name]) / (endObjPos[obj.name] - startObjPos[obj.name]) * obj.y);
+					var posX = ((scrollTop - startObjPos[obj.name]) / (endObjPos[obj.name] - startObjPos[obj.name]) * obj.x);
+				} else {
+					var posY = ((screenEnd - startObjPos[obj.name]) / (endObjPos[obj.name] - startObjPos[obj.name]) * obj.y) / 2;
+					var posX = ((screenEnd - startObjPos[obj.name]) / (endObjPos[obj.name] - startObjPos[obj.name]) * obj.x) / 2;
+				}
+				style['top'] = posY + 'px';
+				style['left'] = posX + 'px';
+			} else if(startObjPos[obj.name] >= screenEnd) {
+				style['top'] = 0;
+				style['left'] = 0;
+			}
+			movObj[obj.name].css(style);
 		}
-		movObj[obj.name].css(style);
 	}
 }
 function scrollClsObj(screenEnd) {
